5fe489a63c17d1a4fbf937d08517b1ebaca56791
Olivier Louvignes <olivier@mgcrea.io>
2023-01-11 01:37:31 +0100

    feat(update): minor changes

---
+++ test/benchmark/cryptoUnseal.ts
@@ -0,0 +1,26 @@
+import benchmark from "benchmark";
+import { SODIUM_AUTH, SODIUM_SECRETBOX } from "src";
+import { secretKey, sodiumAuthFixture, sodiumSecretboxFixture } from "test/fixtures";
+
+const { Suite } = benchmark;
+
+new Suite()
+  .add("SODIUM_SECRETBOX#unsealJson", function () {
+    SODIUM_SECRETBOX.unsealMessage(sodiumSecretboxFixture, [secretKey]);
+  })
+  .add("SODIUM_AUTH#unsealJson", function () {
+    SODIUM_AUTH.unsealMessage(sodiumAuthFixture, [secretKey]);
+  })
+  // .add("HMAC#unsealJson", function () {
+  //   HMAC.unsealMessage(hmacFixture, [secretKey]);
+  // })
+  // add listeners
+  .on("cycle", function (event: Event) {
+    console.log(String(event.target));
+  })
+  .on("complete", function () {
+    // @ts-expect-error this
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+  })
+  // run async
+  .run({ async: true });
