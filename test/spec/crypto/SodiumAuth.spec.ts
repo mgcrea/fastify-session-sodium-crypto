908ea1dc8353d0070a0606d50834c07a3cf8fc3e
Olivier Louvignes <olivier@mgcrea.io>
2023-06-06 23:32:14 +0200

    chore(test): prefer toBe

--- test/spec/crypto/SodiumAuth.spec.ts
+++ test/spec/crypto/SodiumAuth.spec.ts
@@ -8,15 +8,15 @@ describe("SodiumAuth", () => {
     const message = Buffer.from(JSON.stringify({ hello: "world" }));
     const signed = SODIUM_AUTH.sealMessage(message, secretKey);
     expect(signed).toBeDefined();
-    expect(typeof signed).toEqual("string");
-    expect(signed.length).toEqual(69);
-    expect(signed.split(CRYPTO_SPLIT_CHAR).length).toEqual(2);
+    expect(typeof signed).toBe("string");
+    expect(signed.length).toBe(69);
+    expect(signed.split(CRYPTO_SPLIT_CHAR).length).toBe(2);
   });
 
   it("should properly verify a signed message", async () => {
     const result = SODIUM_AUTH.unsealMessage(sodiumAuthFixture, [secretKey]);
     expect(result).toBeDefined();
-    expect(typeof result).toEqual("object");
+    expect(typeof result).toBe("object");
     expect(Object.keys(result)).toEqual(["buffer", "rotated"]);
     expect(Buffer.isBuffer(result.buffer)).toBeTruthy();
     expect(result.buffer.toString("utf8")).toEqual(JSON.stringify({ hello: "world" }));
--- test/spec/crypto/SodiumSecretbox.spec.ts
+++ test/spec/crypto/SodiumSecretbox.spec.ts
@@ -8,14 +8,14 @@ describe("SodiumSecretbox", () => {
     const message = Buffer.from(JSON.stringify({ hello: "world" }));
     const encrypted = SODIUM_SECRETBOX.sealMessage(message, secretKey);
     expect(encrypted).toBeDefined();
-    expect(typeof encrypted).toEqual("string");
-    expect(encrypted.length).toEqual(77);
-    expect(encrypted.split(CRYPTO_SPLIT_CHAR).length).toEqual(2);
+    expect(typeof encrypted).toBe("string");
+    expect(encrypted.length).toBe(77);
+    expect(encrypted.split(CRYPTO_SPLIT_CHAR).length).toBe(2);
   });
   it("should properly decrypt an encrypted message", async () => {
     const result = SODIUM_SECRETBOX.unsealMessage(sodiumSecretboxFixture, [secretKey]);
     expect(result).toBeDefined();
-    expect(typeof result).toEqual("object");
+    expect(typeof result).toBe("object");
     expect(Object.keys(result)).toEqual(["buffer", "rotated"]);
     expect(Buffer.isBuffer(result.buffer)).toBeTruthy();
     expect(result.buffer.toString("utf8")).toEqual(JSON.stringify({ hello: "world" }));
